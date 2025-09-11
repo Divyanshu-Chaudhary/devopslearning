474c70d4880663452ca6f9408e8dcd61cb724e2d
divyanshu <divyanshuchaudhary067@gmail.com>
2025-09-11 16:32:45 +0530

    this is my today's commit

---
+++ game.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    std::cout << "Hello, C++" << std::endl;
+
+    int a = 5;
+    int b = 10;
+    int sum = a + b;
+    cout<<"Sum:"<<sum<<endl;
+
+    cout<<"Enter a number:"<<endl;
+    int num;
+    cin>>num;
+    cout<<"You entered:"<<num<<endl;
+
+    return 0;
+}
